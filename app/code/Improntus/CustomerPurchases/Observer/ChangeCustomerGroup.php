552595dae8b86d47c8d980ddc7d986c7cf41d29d
Alejandro Montagna <alejandromontagna84@gmail.com>
2022-08-17 18:19:06 -0300

    ajuste en cambio de grupo de clientes

--- app/code/Improntus/CustomerPurchases/Observer/ChangeCustomerGroup.php
+++ app/code/Improntus/CustomerPurchases/Observer/ChangeCustomerGroup.php
@@ -1,31 +1,37 @@
 <?php
 namespace Improntus\CustomerPurchases\Observer;
+use \Magento\Store\Model\StoreManagerInterface;
 use \Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\CustomerFactory;
 class ChangeCustomerGroup implements ObserverInterface
 {
     public function __construct(
+        StoreManagerInterface $storeManager,
         CollectionFactory $orderCollectionFactory,
-        CustomerRepositoryInterface $customerRepository
+        CustomerFactory $customerFactory
     ){
+        $this->storeManager = $storeManager;
         $this->orderCollectionFactory = $orderCollectionFactory;
-        $this->customerRepository = $customerRepository;
+        $this->customerFactory = $customerFactory;
     }
 
     public function execute(
         \Magento\Framework\Event\Observer $observer
     ){
+        $websiteId = $this->storeManager->getStore()->getWebsiteId();
+        \Magento\Framework\App\ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class)->debug('website id:'.$websiteId);
         $order = $observer->getEvent()->getOrder();
         $customerId = $order->getCustomerId();
         $ordersByCustomer =  $this->orderCollectionFactory->create();
         $ordersByCustomer->addFieldToFilter('customer_id', $customerId);
         $ordersQty = $ordersByCustomer->count();
-        if ($ordersQty > 5){
-            $customer = $this->customerRepository->getById($customerId);
-            $customer->setData('group_id', 27);
-            $customer->save();
-        }
 
+        $customerGroupId = $ordersQty > 4 ? 5 : 4;
+        $customerFactory = $this->customerFactory->create();
+        $customer = $customerFactory->setWebsiteId($websiteId)->loadByEmail($order->getCustomerEmail());
+
+        $customer->setWebsiteId($websiteId)->setData('group_id', $customerGroupId);
+        $customer->save();
     }
 }
