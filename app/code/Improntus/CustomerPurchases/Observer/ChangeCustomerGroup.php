dfe6039a48453e5d114b14e2b7de35accd412ca5
Alejandro Montagna <alejandromontagna84@gmail.com>
2022-08-17 18:22:15 -0300

    ajuste en cambio de grupo de clientes

--- app/code/Improntus/CustomerPurchases/Observer/ChangeCustomerGroup.php
+++ app/code/Improntus/CustomerPurchases/Observer/ChangeCustomerGroup.php
@@ -20,18 +20,18 @@ class ChangeCustomerGroup implements ObserverInterface
         \Magento\Framework\Event\Observer $observer
     ){
         $websiteId = $this->storeManager->getStore()->getWebsiteId();
-        \Magento\Framework\App\ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class)->debug('website id:'.$websiteId);
+
         $order = $observer->getEvent()->getOrder();
         $customerId = $order->getCustomerId();
-        $ordersByCustomer =  $this->orderCollectionFactory->create();
+        $ordersByCustomer = $this->orderCollectionFactory->create();
         $ordersByCustomer->addFieldToFilter('customer_id', $customerId);
         $ordersQty = $ordersByCustomer->count();
 
-        $customerGroupId = $ordersQty > 4 ? 5 : 4;
+        $newCustomerGroupId = $ordersQty > 4 ? 5 : 4;
+
         $customerFactory = $this->customerFactory->create();
         $customer = $customerFactory->setWebsiteId($websiteId)->loadByEmail($order->getCustomerEmail());
-
-        $customer->setWebsiteId($websiteId)->setData('group_id', $customerGroupId);
+        $customer->setWebsiteId($websiteId)->setData('group_id', $newCustomerGroupId);
         $customer->save();
     }
 }
