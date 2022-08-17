7d6f11ce04aa922283ed7d26bdea3868bda37ac4
Alejandro Montagna <alejandromontagna84@gmail.com>
2022-08-17 17:22:09 -0300

    plugin para actualizar el nombre del cliente

---
+++ app/code/Improntus/CustomerCode/Plugin/Magento/Customer/Model/Customer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Improntus\CustomerCode\Plugin\Magento\Customer\Model;
+
+class Customer
+{
+
+    public function aroundSave(
+        \Magento\Customer\Model\Customer $subject,
+        \Closure $proceed
+    ) {
+
+        $firstname = $subject->getFirstname();
+        $intVal = (int)filter_var($firstname, FILTER_SANITIZE_NUMBER_INT);
+        if ($intVal == 0){
+            $newName = $firstname . '1';
+        } else{
+            $newName = str_replace((string)$intVal, (string)($intVal + 1), $firstname);
+        }
+        $subject->setData('firstname', $newName);
+
+        $result = $proceed();
+        return $result;
+    }
+}
