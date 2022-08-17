1598c3f88e18c10252c76ee74cf8866957ea9e4b
Alejandro Montagna <alejandromontagna84@gmail.com>
2022-08-17 16:51:26 -0300

    fix constructor del observer

--- app/code/Improntus/CustomerCode/Observer/AddCustomerData.php
+++ app/code/Improntus/CustomerCode/Observer/AddCustomerData.php
@@ -1,33 +1,28 @@
 <?php
-
 namespace Improntus\CustomerCode\Observer;
-
 use Magento\Framework\Event\ObserverInterface;
-use \Magento\Customer\Api\CustomerRepositoryInterface;
 use \Improntus\CustomerCode\Model\CustomerFactory;
 class AddCustomerData implements ObserverInterface
 {
     public function __construct(
-        CustomerRepositoryInterface $customerRepositoryInterface,
         CustomerFactory $customerFactory
     ){
-        $this->customerRepositoryInterface = $customerRepositoryInterface;
         $this->customerFactory = $customerFactory;
     }
 
-    public function execute(\Magento\Framework\Event\Observer $observer)
-    {
+    public function execute(
+        \Magento\Framework\Event\Observer $observer
+    ){
         $customer = $observer->getEvent()->getCustomer();
         $id = $customer->getId();
         $model = $this->customerFactory->create();
 
-
+        $data['code'] = md5($id);
         $data['customer_id'] = $id;
         $data['created_at'] = time();
 
         $model->setData($data);
 
         $model->save();
-        //\Magento\Framework\App\ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class)->debug($customer->getFirstname());
     }
 }
